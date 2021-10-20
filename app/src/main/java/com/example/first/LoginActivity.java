48d734a7230b200178a335b2341b32ba50ac62b5
Ernesto Ramiro <atomms@gmail.com>
2021-10-20 18:17:33 +0200

    glide on

--- app/src/main/java/com/example/first/LoginActivity.java
+++ app/src/main/java/com/example/first/LoginActivity.java
@@ -1,14 +1,14 @@
 package com.example.first;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 /**
  * @author eramiro
@@ -20,22 +20,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-//    ImageView mForest = findViewById(R.id.backforest);
-//
-////    New Glide
-//        Glide.with(this)
-//                .load(R.drawable.mforest)
+
+//    Glide for loading girls
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(500))
+                .into(mGirl);
+
 //                .centerCrop()
-//                .transition(DrawableTransitionOptions.withCrossFade(500))
-//            .placeholder(new ColorDrawable(this.getResources().getColor(R.color.light_purple_button)))
-////                .circleCrop()
-////                .diskCacheStrategy(DiskCacheStrategy.NONE)
-//            .into(mForest);
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
+//                .circleCrop()
+//                .diskCacheStrategy(DiskCacheStrategy.NONE)
+    }
 
-
-
-    public void onClick(View v){
+    // Method for Vaccinate button
+    public void onClick(View v) {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
