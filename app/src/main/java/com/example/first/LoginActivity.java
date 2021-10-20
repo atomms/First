89ce4305f228bc10ee566aea5d68d1141d3c8f3b
eramiro <atomms@gmail.com>
2021-10-20 13:59:56 +0200

    gliding

--- app/src/main/java/com/example/first/LoginActivity.java
+++ app/src/main/java/com/example/first/LoginActivity.java
@@ -2,7 +2,13 @@ package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * @author eramiro
@@ -15,4 +21,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+//    ImageView mForest = findViewById(R.id.backforest);
+//
+////    New Glide
+//        Glide.with(this)
+//                .load(R.drawable.mforest)
+//                .centerCrop()
+//                .transition(DrawableTransitionOptions.withCrossFade(500))
+//            .placeholder(new ColorDrawable(this.getResources().getColor(R.color.light_purple_button)))
+////                .circleCrop()
+////                .diskCacheStrategy(DiskCacheStrategy.NONE)
+//            .into(mForest);
+
+
+
+    public void onClick(View v){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
+
