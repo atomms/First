2b2af37ad9107b8a02be7227dd6914865760d108
Ernesto Ramiro <atomms@gmail.com>
2021-10-25 11:05:53 +0200

    splashing

--- app/src/main/java/com/example/first/LoginActivity.java
+++ app/src/main/java/com/example/first/LoginActivity.java
@@ -42,8 +42,8 @@ public class LoginActivity extends AppCompatActivity {
     // Method for Vaccinate button
     public void onClick(View v) {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
