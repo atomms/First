ee9c7facccfdf54b5299fa21928a3c4c44d81310
Ernesto Ramiro <atomms@gmail.com>
2021-11-03 20:41:34 +0100

    action ON

--- app/src/main/java/com/example/first/Main.java
+++ app/src/main/java/com/example/first/Main.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +25,37 @@ public class Main extends AppCompatActivity {
         registerForContextMenu(mycontext);
 
     }
+
+    //implementing ActionBar/AppBar menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.item1) {
+            Toast toast = Toast.makeText(this,"going item1",Toast.LENGTH_LONG );
+            toast.show();
+
+        }
+        if (id == R.id.item2) {
+            Toast toast = Toast.makeText(this,"going item2",Toast.LENGTH_LONG );
+            toast.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    // implementing context menu
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
