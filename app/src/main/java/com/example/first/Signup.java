9be92baca5fb1d1fb773e8f10cc21e69872b148d
Ernesto Ramiro <atomms@gmail.com>
2022-01-17 09:40:14 +0100

    signup big lands

--- app/src/main/java/com/example/first/Signup.java
+++ app/src/main/java/com/example/first/Signup.java
@@ -2,8 +2,13 @@ package com.example.first;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
@@ -20,5 +25,18 @@ public class Signup extends AppCompatActivity {
 
         //added a transparent shape to the traditional action bar
 //        getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.transparent));
+
+        TextView dateView = (TextView)findViewById(R.id.mydate);
+        setDate(dateView);
+
+    }
+
+    public void setDate (TextView view){
+//        String str = String.format("%tc", new Date());
+        Date today = Calendar.getInstance().getTime();//getting date
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");//formating according to my need
+        String date = formatter.format(today);
+        view.setText(date);
+//        view.setText(str);
     }
 }
