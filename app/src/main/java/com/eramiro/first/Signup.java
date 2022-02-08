ebe42f972c548b35404617f413b30df90c2cec0f
eramiro <atomms@gmail.com>
2022-02-08 09:16:28 +0100

    anycharting

--- app/src/main/java/com/eramiro/first/Signup.java
+++ app/src/main/java/com/eramiro/first/Signup.java
@@ -5,11 +5,17 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.eramiro.first.R;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * This is the Signup
@@ -29,6 +35,17 @@ public class Signup extends AppCompatActivity {
 
         TextView dateView = (TextView) findViewById(R.id.mydate);
         setDate(dateView);
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.anychartview);
+        anyChartView.setChart(pie);
+
 
     }
 
