c356ac524bb0e20e2d427d8e33fe0f5fc0f5c126
eramiro <atomms@gmail.com>
2022-02-08 10:05:47 +0100

    anychart pie and funnel

--- app/src/main/java/com/eramiro/first/Signup.java
+++ app/src/main/java/com/eramiro/first/Signup.java
@@ -9,7 +9,7 @@ import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Funnel;
+import com.anychart.charts.Pie;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -36,37 +36,40 @@ public class Signup extends AppCompatActivity {
         TextView dateView = (TextView) findViewById(R.id.mydate);
         setDate(dateView);
 
-//        Pie pie = AnyChart.pie();
-//
-//        List<DataEntry> data = new ArrayList<>();
-//        data.add(new ValueDataEntry("John", 10000));
-//        data.add(new ValueDataEntry("Jake", 12000));
-//        data.add(new ValueDataEntry("Peter", 18000));
-//
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.anychartview);
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> datat = new ArrayList<>();
+        datat.add(new ValueDataEntry("John", 10000));
+        datat.add(new ValueDataEntry("Jake", 12000));
+        datat.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(datat);
+
 //        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.anychartview);
-//        anyChartView.setChart(pie);
+        anyChartView.setChart(pie);
 
 
-        final AnyChartView funnelchartPayable1=  findViewById(R.id.anychartview);
-        final Funnel funnel1 = AnyChart.funnel();
-        List<DataEntry> dataf = new ArrayList<>();
-        dataf.add(new ValueDataEntry("Website Visits", 528756));
-        dataf.add(new ValueDataEntry("Downloads", 164052));
-        dataf.add(new ValueDataEntry("Valid Contacts", 112167));
-        dataf.add(new ValueDataEntry("Interested to Buy", 79128));
-        dataf.add(new ValueDataEntry("Purchased", 79128));
-
-        funnel1.data(dataf);
-        funnel1.margin(new String[]{"10", "20%", "10", "20%"});
-        //   funnel1.baseWidth("70%")
-        //         .neckWidth("17%");
-
-        funnel1.labels()
-                .position("outsideleft")
-                .format("{%X} - {%Value}");
-        funnel1.animation(true, 800);
-
-        funnelchartPayable1.setChart(funnel1);
+//        final AnyChartView funnelchartPayable1=  findViewById(R.id.anychartview);
+//        final Funnel funnel1 = AnyChart.funnel();
+//        List<DataEntry> dataf = new ArrayList<>();
+//        dataf.add(new ValueDataEntry("Website Visits", 528756));
+//        dataf.add(new ValueDataEntry("Downloads", 164052));
+//        dataf.add(new ValueDataEntry("Valid Contacts", 112167));
+//        dataf.add(new ValueDataEntry("Interested to Buy", 79128));
+//        dataf.add(new ValueDataEntry("Purchased", 79128));
+//
+//        funnel1.data(dataf);
+//        funnel1.margin(new String[]{"10", "20%", "10", "20%"});
+//        //   funnel1.baseWidth("70%")
+//        //         .neckWidth("17%");
+//
+//        funnel1.labels()
+//                .position("outsideleft")
+//                .format("{%X} - {%Value}");
+//        funnel1.animation(true, 800);
+//
+//        funnelchartPayable1.setChart(funnel1);
 
 
     }
