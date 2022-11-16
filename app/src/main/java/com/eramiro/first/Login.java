c33f68ba99fe88ffa3c50a1a8b82600b1aba9c35
eramiro <atomms@gmail.com>
2022-11-16 10:28:26 +0100

    shaking with 50cycles interpolator

--- app/src/main/java/com/eramiro/first/Login.java
+++ app/src/main/java/com/eramiro/first/Login.java
@@ -34,7 +34,7 @@ public class Login extends AppCompatActivity {
 
         //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
         // startAnimation lo aplicamos al imageview del logo
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.shake);
         logo.startAnimation(myanim);
 
 
