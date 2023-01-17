196d8f007dd5dba43fc7b9dbc9bc5c2c26a18082
Ernesto Ramiro <atomms@gmail.com>
2023-01-17 08:30:30 +0100

    no binding anymore

--- app/src/main/java/com/eramiro/first/MainBn.java
+++ app/src/main/java/com/eramiro/first/MainBn.java
@@ -21,7 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainBn extends AppCompatActivity {
 
-    private ActivityMainBnBinding binding;
+//    private ActivityMainBnBinding binding;
     private MenuItem prevMenuItem;
     private SectionsPagerAdapter sectionsPagerAdapter;
 
@@ -29,8 +29,8 @@ public class MainBn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_bn);
-        binding = ActivityMainBnBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+//        binding = ActivityMainBnBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
 
         //el adaptador coloca las Pages -los fragmentos con las diferentes vistas- dentro de la vista padre Viewpager del xml
         sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
