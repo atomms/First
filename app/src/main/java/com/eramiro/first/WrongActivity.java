be34510d297faca136df18da76eb2de3e540668b
Ernesto Ramiro <atomms@gmail.com>
2023-10-23 12:21:28 +0200

    constraining

---
+++ app/src/main/java/com/eramiro/first/WrongActivity.java
@@ -0,0 +1,14 @@
+package com.eramiro.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WrongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrong);
+    }
+}
