2c0097de00dc55b2b8faafb856d80680b7597d17
Ernesto Ramiro <atomms@gmail.com>
2023-10-23 14:49:07 +0200

    login with tractor refactor

--- app/src/main/java/com/eramiro/first/LoginAlter.java
+++ app/src/main/java/com/eramiro/first/LoginAlter.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class WrongActivity extends AppCompatActivity {
+public class LoginAlter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wrong);
+        setContentView(R.layout.activity_loginalter);
     }
 }
