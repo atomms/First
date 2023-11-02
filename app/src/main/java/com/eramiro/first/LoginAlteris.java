2f64e4fbcd3747779b358fa402e2f08867b65cac
Ernesto Ramiro <eramiro@informatica.iesjc>
2023-11-02 13:14:59 +0100

    added hipman.png and lands

--- app/src/main/java/com/eramiro/first/LoginAlteris.java
+++ app/src/main/java/com/eramiro/first/LoginAlteris.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginAlter extends AppCompatActivity {
+public class LoginAlteris extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginalter);
+        setContentView(R.layout.activity_loginalteris);
     }
 }
