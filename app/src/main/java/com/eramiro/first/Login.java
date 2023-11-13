6619e8fb33e0c851aa7d9c57ff9f380c4c3de7a7
Ernesto Ramiro <atomms@gmail.com>
2023-11-13 14:15:51 +0100

    glide version 16 and new girl

--- app/src/main/java/com/eramiro/first/Login.java
+++ app/src/main/java/com/eramiro/first/Login.java
@@ -43,10 +43,12 @@ public class Login extends AppCompatActivity {
 //        ImageView mLogo = findViewById(R.id.logo);
 
         Glide.with(this)
-              .load(R.drawable.girl)
-//                .transition(DrawableTransitionOptions.withCrossFade(2000))
-//                .centerCrop()
-//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+//              .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+//                .circleCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
                 .into(mGirl);
 
 //        Glide.with(this)
