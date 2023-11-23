82f2c257418458012c54bc67dc9b60cd5f2058a2
Zan-40 <alexrodriguezmazariegos@gmail.com>
2023-11-23 13:36:13 +0100

    new animation

--- app/src/main/java/com/eramiro/first/Splash.java
+++ app/src/main/java/com/eramiro/first/Splash.java
@@ -11,6 +11,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -25,57 +26,28 @@ import com.eramiro.first.R;
 
 public class Splash extends AppCompatActivity {
 
+    private LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        lottie = findViewById(R.id.lottie);
+
         openApp();
-
-
-        //implements and starts animation
-        // objeto thunder sobre el cual aplicaremos la animación
-        ImageView thunder = findViewById(R.id.logosplash);
-
-        //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
-        // startAnimation lo aplicamos al imageview del logo
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
-        thunder.startAnimation(myanim);
-
-
-        //    Glide for loading girls
-        ImageView mSea = findViewById(R.id.backView);
-
-        Glide.with(this)
-              .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
-//                .load(R.drawable.girl)
-                .transition(DrawableTransitionOptions.withCrossFade(100))
-                .centerCrop()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
-//                .circleCrop()
-                .into(mSea);
-
     }
 
-    private void openApp() {
-
+    private void openApp(){
         Handler handler = new Handler();
         handler.postDelayed(new Runnable()
         {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash
-                        .this, Login.class);
+                Intent intent = new Intent(Splash.this, Login.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-
-
-
             }
-        }, 5000);
-
-
+        }, 2000);
     }
 
 }
